14f2385cdb33a7d0dab19581970fff3b748e4ae0
Maxim Gabidullin <after@ya.ru>
2020-07-30 15:11:51 +0300

    service

---
+++ service/ParseLogIntoDb.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\service;
+
+
+use app\entity\logrow\LogRowInterface;
+
+class ParseLogIntoDb
+{
+    /**
+     * @param LogRowInterface[] $rows
+     */
+    public function logIntoDb(array $rows)
+    {
+
+    }
+}
